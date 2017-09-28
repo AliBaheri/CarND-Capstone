04e07b968ede4e0990e7bfacb25b826a1bbf4fa3
Celien Nanson <c.nanson@sofort.com>
2017-09-28 14:10:09 +0200

    Incorporate changes from Anu

--- ros/src/twist_controller/dbw_node.py
+++ ros/src/twist_controller/dbw_node.py
@@ -61,6 +61,8 @@ class DBWNode(object):
 		self.current_pose = None # Commig from /current_pose
 		self.prev_throttle = 0
 		self.target_velocity = None
+		self.prev_steering = 0
+ 		self.prev_brake = 0
 
 		# Define Publishers
 		self.steer_pub = rospy.Publisher('/vehicle/steering_cmd', SteeringCmd, queue_size=1)
@@ -107,20 +109,26 @@ class DBWNode(object):
 		All the parameters values should be in the range [0,1] as per Udacity requirements
 		"""
 		tcmd = ThrottleCmd()
-		tcmd.enable = True
+		tcmd.enable = (abs(throttle - self.prev_throttle) > 0.05)
+		if tcmd.enable :
+			self.prev_throttle = throttle
 		tcmd.pedal_cmd_type = ThrottleCmd.CMD_PERCENT
 		tcmd.pedal_cmd = throttle
 		rospy.loginfo(throttle)
 		self.throttle_pub.publish(tcmd)
 
 		scmd = SteeringCmd()
-		scmd.enable = True
+		scmd.enable = (abs(steer - self.prev_steering) > 0.05)
+		if scmd.enable :
+			self.prev_steering = steer
 		scmd.steering_wheel_angle_cmd = steer
 		rospy.loginfo(steer)
 		self.steer_pub.publish(scmd)
 
 		bcmd = BrakeCmd()
-		bcmd.enable = True
+		bcmd.enable = (abs(brake - self.prev_brake) > 0.05)
+		if bcmd.enable :
+			self.prev_brake = brake
 		bcmd.pedal_cmd_type = BrakeCmd.CMD_TORQUE
 		bcmd.pedal_cmd = brake
 		rospy.loginfo(brake)
