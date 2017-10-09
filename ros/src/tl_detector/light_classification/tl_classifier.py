631c1c560cd8b9676fcdfb2518fda19c8f172456
Jason Scott <jason@jaylin.com.au>
2017-10-09 09:03:49 +0800

    changes for classifier to work on carla

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -11,25 +11,26 @@ from io import StringIO
 from utilities import label_map_util
 from utilities import visualization_utils as vis_util
 
-#import cv2
-
-#Testing
-#from matplotlib import pyplot as plt
-
 
 class TLClassifier(object):
     def __init__(self, *args):
-
-        print("Classifier launched in site mode : ", args[0])
+        self.RUNNING_ON_CARLA = args[0]
+        print("Classifier launched in site mode : ", self.RUNNING_ON_CARLA)
 
         self.current_light = TrafficLight.RED
         cwd = os.path.dirname(os.path.realpath(__file__))
-
-        # Path to frozen detection graph. This is the actual model that is used for the object detection.
         base_path = os.path.dirname(os.path.abspath(__file__))
-        #MODEL_NAME = 'ssd_mobilenet_v1_coco_11_06_2017'
-        MODEL_NAME = 'ssd_mobilenet_sim'
-        PATH_TO_CKPT = os.path.join(base_path, MODEL_NAME, 'frozen_inference_graph.pb')
+
+        if self.RUNNING_ON_CARLA == True:
+            # This is a large frozen model and must be downloaded separately from this github
+            # repository from: http://download.tensorflow.org/models/object_detection/faster_rcnn_resnet101_coco_11_06_2017.tar.gz
+            # Extract with `tar -xzvf faster_rcnn_resnet101_coco_11_06_2017`
+            MODEL_NAME = 'download_faster_rcnn_resnet101_coco_11_06_2017'
+            PATH_TO_CKPT = os.path.join(base_path, MODEL_NAME, 'frozen_inference_graph.pb')
+        else:
+            MODEL_NAME = 'ssd_mobilenet_sim'
+            PATH_TO_CKPT = os.path.join(base_path, MODEL_NAME, 'frozen_inference_graph.pb')
+
         #CHUNK_SIZE = 10485760  # 10MB
         #PATH_TO_CHUNKS = os.path.join(base_path, MODEL_NAME, 'chunks')
 
@@ -50,14 +51,20 @@ class TLClassifier(object):
         #    output.close()
 
         # Load label map
-        PATH_TO_LABELS = os.path.join(base_path, 'data', 'traffic_lights_label_map.pbtxt')
-        NUM_CLASSES = 14
+        self.RUNNING_ON_CARLA:
+            PATH_TO_LABELS = os.path.join(base_path, 'data', 'mscoco_label_map.pbtxt')
+            NUM_CLASSES = 90
+        else:
+            PATH_TO_LABELS = os.path.join(base_path, 'data', 'traffic_lights_label_map.pbtxt')
+            NUM_CLASSES = 14
+
         label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
         categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES,
                                                                     use_display_name=True)
         self.category_index = label_map_util.create_category_index(categories)
 
-        # Build network
+
+        # Load and build network
         self.detection_graph = tf.Graph()
         config = tf.ConfigProto()
         config.gpu_options.allow_growth = True  # https://github.com/tensorflow/tensorflow/issues/6698
@@ -109,27 +116,56 @@ class TLClassifier(object):
         scores = np.squeeze(scores)
         classes = np.squeeze(classes).astype(np.int32)
 
-        # Check the detections. If it has a good score
-        # then set the current light to the detected label. The
-        # first one is alwasy the best (they are returned sorted 
-        # in score order).
-        # Note that we have trained for 14 categories, including
-        # left/right arrows etc. Here we are only looking for 
-        # standard red, yellow and green light and ignore others.
-        for i in range(boxes.shape[0]):
-            if scores is None or scores[i] > .05:
-                classname = self.category_index[classes[i]]['name']
-                print(classname, scores[i])
+        if self.RUNNING_ON_CARLA == True:
+            for i in range(boxes.shape[0]):
+                if scores is None or scores[i] > .05:
+                    if classes[i] == 10:
+                    classname = category_index[classes[i]]['name']
+                    print(classname, scores[i])
+                
+                    # Extract image from best bounding box and pass through light classifier
+                    ymin, xmin, ymax, xmax = boxes[i]
+                    im_height, im_width, im_depth = image_np.shape
+                    (left, right, top, bottom) = (xmin * im_width, xmax * im_width, ymin * im_height, ymax * im_height)
+                    tf_image_cropped = image_np[int(top):int(bottom), int(left):int(right), :]
 
-                if classname == 'Green':
-                    self.current_light = TrafficLight.GREEN
-                elif classname == 'Yellow':
-                    self.current_light = TrafficLight.YELLOW
-                elif classname == 'Red':
-                    self.current_light = TrafficLight.RED
-                else:
-                    self.current_light = TrafficLight.UNKNOWN
+                    PILImage = Image.fromarray(tf_image_cropped)
+                    resized_img = PILImage.resize((85, 256), Image.ANTIALIAS)
+                    image_np_resized = load_image_into_numpy_array(resized_img)
+                    x = np.expand_dims(image_np_resized, axis=0)
+                    x = np.vstack([x])
 
-            break
+                    model = load_model('tf_classifier_1.h5')
+                    model.compile(loss='categorical_crossentropy',
+                                  optimizer='adam',
+                                  metrics=['accuracy'])
+                    classes = model.predict_classes(x, batch_size=1)
+                    print(classes)
+                
+                    break
+
+        else:
+            # Check the detections. If it has a good score
+            # then set the current light to the detected label. The
+            # first one is always the best (they are returned sorted 
+            # in score order).
+            # Note that we have trained for 14 categories, including
+            # left/right arrows etc. Here we are only looking for 
+            # standard red, yellow and green light and ignore others.
+            for i in range(boxes.shape[0]):
+                if scores is None or scores[i] > .05:
+                    classname = self.category_index[classes[i]]['name']
+                    print(classname, scores[i])
+
+                    if classname == 'Green':
+                        self.current_light = TrafficLight.GREEN
+                    elif classname == 'Yellow':
+                        self.current_light = TrafficLight.YELLOW
+                    elif classname == 'Red':
+                        self.current_light = TrafficLight.RED
+                    else:
+                        self.current_light = TrafficLight.UNKNOWN
+
+                break
 
         return self.current_light
